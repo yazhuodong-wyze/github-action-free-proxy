30cd78ada34accd179954087ad3193ab888a1621
yazhuo.dong <yazhuo.dong@wyze.cn>
2022-10-23 20:10:21 +0800

    first commit.

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ echo $key
 mkdir -p $HOME/.ssh/
 cp id_rsa.pub $HOME/.ssh/
 
-autossh -P 11626 -M 5678 -CNR 8888:0.0.0.0:8888 pi@y25a350891.qicp.vip
+autossh -p 11626 -M 5678 -CNR 8888:0.0.0.0:8888 pi@y25a350891.qicp.vip
 
 #sshpass -pmyqq.com scp -P 11626 date.txt pi@y25a350891.qicp.vip:/home/pi/
 
