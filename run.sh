e08d28e6df27718c146b81573640c3c9ff340aa0
yazhuo.dong <yazhuo.dong@wyze.cn>
2022-10-23 21:51:05 +0800

    first commit.

--- run.sh
+++ run.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 sudo apt install autossh tinyproxy
+echo '下载必要组件完成'
 sudo service tinyproxy restart
-
+echo '启动代理完成'
 
 mkdir -p $HOME/.ssh/
-cp ./secrets/known_hosts $HOME/.ssh/
-chmod 0600 ./secrets/id_rsa.pub
-#chmod 0600 $HOME/.ssh/id_rsa
+cp ./secrets/* $HOME/.ssh/
+chmod 0600 $HOME/.ssh/id_rsa
+echo '密钥拷贝完成'
 
-echo '拷贝完成'
-
-autossh -p 11626 -M 5678 -CNR 8888:0.0.0.0:8888 pi@y25a350891.qicp.vip -i ./secrets/id_rsa.pub
+autossh -p 11626 -M 5678 -CNR 8888:0.0.0.0:8888 pi@y25a350891.qicp.vip &
+echo '隧道打通完成'
+nc -l 54321
 
