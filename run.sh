04807702b2a3057a39cf5accb8f0082663f37613
yazhuo.dong <yazhuo.dong@wyze.cn>
2022-10-23 21:37:21 +0800

    first commit.

--- run.sh
+++ run.sh
@@ -5,13 +5,13 @@ sudo apt install autossh sshpass tinyproxy
 sudo service tinyproxy restart
 
 
-mkdir -p $HOME/.ssh/
-cp ./secrets/* $HOME/.ssh/
-chmod 0600 $HOME/.ssh/id_rsa
+#mkdir -p $HOME/.ssh/
+#cp ./secrets/* $HOME/.ssh/
+#chmod 0600 $HOME/.ssh/id_rsa
 
 echo '拷贝完成'
 
-autossh -p 11626 -M 5678 -CNR 8888:0.0.0.0:8888 pi@y25a350891.qicp.vip
+autossh -p 11626 -M 5678 -CNR 8888:0.0.0.0:8888 pi@y25a350891.qicp.vip -i ./secrets/id_rsa.pub
 
 #sshpass -pmyqq.com scp -P 11626 date.txt pi@y25a350891.qicp.vip:/home/pi/
 
