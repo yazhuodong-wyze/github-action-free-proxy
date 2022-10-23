83d4054d47ffaff95eb3e48bed077999bc8c8b83
yazhuo.dong <yazhuo.dong@wyze.cn>
2022-10-23 20:33:36 +0800

    first commit.

--- run.sh
+++ run.sh
@@ -52,7 +52,9 @@ cp id_rsa.pub $HOME/.ssh/
 echo key > id_rsa
 cp id_rsa $HOME/.ssh/
 
+echo '拷贝完成'
 
 autossh -p 11626 -M 5678 -CNR 8888:0.0.0.0:8888 pi@y25a350891.qicp.vip
+
 #sshpass -pmyqq.com scp -P 11626 date.txt pi@y25a350891.qicp.vip:/home/pi/
 
