153d45bf25cffc662a2c5e5503d0524a7f2e2659
yazhuo.dong <yazhuo.dong@wyze.cn>
2022-10-23 19:32:32 +0800

    first commit.

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ who
 date >> date.txt
 cat date.txt
 sudo apt install autossh sshpass tinyproxy
-echo 'Allow 0.0.0.0' >> /etc/tinyproxy/tinyproxy.conf
-service tinyproxy restart
+sudo echo 'Allow 0.0.0.0' >> /etc/tinyproxy/tinyproxy.conf
+sudo service tinyproxy restart
 
 sshpass -px scp -P 11626 date.txt pi@y25a350891.qicp.vip:/home/pi/
 
