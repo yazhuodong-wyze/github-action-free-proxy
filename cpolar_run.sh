df97390dc2962983a07ff7c8d611100ce4165774
YazhuoDong <106308779+yazhuodong-wyze@users.noreply.github.com>
2022-10-27 18:04:01 +0800

    Delete cpolar_run.sh

--- cpolar_run.sh
+++
@@ -1,5 +0,0 @@
-sudo apt --force-yes -y install tinyproxy 
-sudo service tinyproxy restart
-sudo curl -L https://www.cpolar.com/static/downloads/install-release-cpolar.sh | sudo bash
-cpolar authtoken $CPOLAR_AUTHTOKEN
-cpolar tcp 8888
