7bbc4aa0a0f12e47e0bd39ef251812836d44fe0c
YazhuoDong <106308779+yazhuodong-wyze@users.noreply.github.com>
2022-10-27 17:49:07 +0800

    Update cpolar_run.sh

--- cpolar_run.sh
+++ cpolar_run.sh
@@ -1,7 +1,7 @@
+cpolar_authtoken='ZDY3ZjhhZjUtY2Q2ZS00NzliLTk1MjItMDU3ZTczOWMyMjJk'
+
 sudo apt --force-yes -y install tinyproxy 
 sudo service tinyproxy restart
 sudo curl -L https://www.cpolar.com/static/downloads/install-release-cpolar.sh | sudo bash
-cpolar version
-cpolar authtoken ZDY3ZjhhZjUtY2Q2ZS00NzliLTk1MjItMDU3ZTczOWMyMjJk
-cpolar tcp 8888 &
-nc -l 54321
+cpolar authtoken ${cpolar_authtoken}
+cpolar tcp 8888
